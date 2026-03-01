60bc1a01e4f6ec2b7a699122f250151358cfc5b8
wigek <wilder.garciaramirez@gmail.com>
2026-03-01 07:37:26 -0500

    Feature: Implementación ciclo while en main

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+from src.registro import registro
+
 def menuPrincipal():
     while True:
         print("Bienvenido a Edulend \n Selecciona una de las siguientes opciones: \n 1. Registrar usuario \n 2. Inicio de sesion usuario \n 3. Inicio de sesion como administrador")
-    
+        
+        opcion= input("\n Seleeciona una opción:")
+        if opcion == "1":
+            registro()
+            print("\n Registrando...")
+        elif opcion =="2":
+            
+        elif opcion =="3":
+            
+        elif opcion =="4":
+            
+        else:
+            print("\n[!] Opción no válida. Inténtalo de nuevo.")
+            input("Presiona Enter para continuar...")
